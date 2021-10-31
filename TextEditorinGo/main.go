6fff71c88e6024a7f18e28ce17a02b3f35cc06ba
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-11-01 00:17:51 +0530

    commit to master

--- TextEditorinGo/main.go
+++ TextEditorinGo/main.go
@@ -1,8 +1,22 @@
 package main
 
+import(
+
+   "fyne.io/fyne/v2"
+   "fyne.io/fyne/v2/app"
+
+)
+
+
 
 func main() {
 
+  a:= app.New()
 
-	
+  w:=a.NewWindow("Text Editor")
+
+  w.Resize(fyne.NewSize(600, 600))
+
+  w.ShowAndRun()
+
 }
