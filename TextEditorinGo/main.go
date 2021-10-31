6177f3725b7cb45877c2b4944e54103bd3d55194
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-11-01 00:53:36 +0530

    commit to master

--- TextEditorinGo/main.go
+++ TextEditorinGo/main.go
@@ -1,11 +1,16 @@
 package main
 
 import(
-
+   "strconv"
    "fyne.io/fyne/v2"
    "fyne.io/fyne/v2/app"
+   "fyne.io/fyne/v2/container"
+   "fyne.io/fyne/v2/widget"
+   
 
 )
+
+var count int = 1
 
 
 
@@ -16,6 +21,51 @@ func main() {
   w:=a.NewWindow("Text Editor")
 
   w.Resize(fyne.NewSize(600, 600))
+
+  content := container.NewVBox(
+	  container.NewHBox(
+		  widget.NewLabel("Text Editor"),
+
+	  ),
+
+  )
+
+  content.Add(widget.NewButton("Add New File", func(){
+	  content.Add(widget.NewLabel("New File" + strconv.Itoa(count)))
+      count++
+	}))
+
+
+	input := widget.NewMultiLineEntry()
+	input.SetPlaceHolder("Enter text...")
+
+	input.Resize(fyne.NewSize(400, 400))
+
+	saveBtn:= widget.NewButton("Save  text File" , func(){
+        saveFileDialog := dialog.NewFileSave(
+			func(uc fyne.URIWriteCloser, _ error) {
+				textData:= []byte(input.Text)
+
+				uc.Write(textData)
+
+			},w)
+
+		saveFileDialog.SetFileName("New File" + strconv.Itoa(count-1)+".txt")
+
+		saveFileDialog.Show()
+
+	})
+
+  w.SetContent(
+	container.NewVBox(
+		content,
+		input,
+
+		container.NewHBox(
+           saveBtn,
+		),
+	),
+)
 
   w.ShowAndRun()
 
