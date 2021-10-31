6a16b24900eaefd79d8bf19cd2b73f68f451c492
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-10-31 15:18:01 +0530

    created all the buttons with naming and updateing clear buttons

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Calculator with Go and Fyne")
 
+    output:=""
+	input := widget.NewLabel(output)
 
 
 	historyBtn:=widget.NewButton("history", func() {
@@ -21,57 +23,86 @@ func main() {
 	})
 	clearBtn:= widget.NewButton("clear" , func() {})
 
-	openBtn:= widget.NewButton("clear" , func() {})
+	openBtn:= widget.NewButton("(" , func() {})
 
-	closeBtn:= widget.NewButton("clear" , func() {})
+	closeBtn:= widget.NewButton(")" , func() {})
 
-	divideBtn:= widget.NewButton("clear" , func() {})
+	divideBtn:= widget.NewButton("/" , func() {})
 
-	sevenBtn:= widget.NewButton("clear" , func() {})
+	sevenBtn:= widget.NewButton("7" , func() {})
 
 
-	eightBtn:= widget.NewButton("clear" , func() {})
+	eightBtn:= widget.NewButton("8" , func() {})
 
-	nineBtn:= widget.NewButton("clear" , func() {})
+	nineBtn:= widget.NewButton("9" , func() {})
 
-	multiplyBtn:= widget.NewButton("clear" , func() {})
+	multiplyBtn:= widget.NewButton("*" , func() {})
     
 
-	fourBtn:= widget.NewButton("clear" , func() {})
+	fourBtn:= widget.NewButton("4" , func() {})
 
-	fiveBtn:= widget.NewButton("clear" , func() {})
+	fiveBtn:= widget.NewButton("5" , func() {})
 
-	sixBtn:= widget.NewButton("clear" , func() {})
+	sixBtn:= widget.NewButton("6" , func() {})
 
-	minusBtn:= widget.NewButton("clear" , func() {})
-
-
-
-	oneBtn:= widget.NewButton("clear" , func() {})
-
-	twoBtn:= widget.NewButton("clear" , func() {})
-
-	threeBtn:= widget.NewButton("clear" , func() {})
-
-	plusBtn:= widget.NewButton("clear" , func() {})
+	minusBtn:= widget.NewButton("-" , func() {})
 
 
 
-	zeroBtn:= widget.NewButton("clear" , func() {})
+	oneBtn:= widget.NewButton("1" , func() {})
 
-	dotBtn:= widget.NewButton("clear" , func() {})
+	twoBtn:= widget.NewButton("2" , func() {})
 
-	equalBtn:= widget.NewButton("clear" , func() {})
+	threeBtn:= widget.NewButton("3" , func() {})
+
+	plusBtn:= widget.NewButton("+" , func() {})
+
+
+
+	zeroBtn:= widget.NewButton("0" , func() {})
+
+	dotBtn:= widget.NewButton("." , func() {})
+
+	equalBtn:= widget.NewButton("=" , func() {})
 
 	
 
-	hello := widget.NewLabel("Hello Fyne!")
+	
 	w.SetContent(container.NewVBox(
 		hello,
-		widget.NewButton("Hello World!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
+		container.NewGridWithColumns(2,
+		historyBtn,
+	    backBtn,
+	    ),
+		container.NewGridWithColumns(4,
+		clearBtn,
+		openBtn,
+		closeBtn,
+		divideBtn,),
+
+		container.NewGridWithColumns(4,
+		nineBtn,
+	    eightBtn,
+        sevenBtn,
+        multiplyBtn,),
+        container.NewGridWithColumns(4,
+         fourBtn,
+		 fiveBtn,
+	     sixBtn,
+         minusBtn,),
+		 container.NewGridWithColumns(4,
+		oneBtn,
+		twoBtn,
+		threeBtn,
+		plusBtn,),
+		container.NewGridWithColumns(2,
+		container.NewGridWithColumns(2,
+		zeroBtn,
+		dotBtn,),
+		equalBtn,),	
+		),	
+	)
+		
 
 	w.ShowAndRun()
 }
