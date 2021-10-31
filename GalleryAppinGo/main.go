0fd768943db02838841461c2c95185f3ff856575
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-10-31 18:07:58 +0530

    initialize rtab 1 and tab 2

--- GalleryAppinGo/main.go
+++ GalleryAppinGo/main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"fmt"
+    "io/ioutil"
+    "log"
+	"strings"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	//"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	//"fyne.io/fyne/v2/theme"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
+	w.Resize(fyne.NewSize(800,600));
+	root_src:="C:\\Users\\HP\\Pictures";
+	files, err := ioutil.ReadDir(root_src);
+	if err != nil {
+        log.Fatal(err)
+    }
+    var picsArr []string;
+	for _, file := range files {
+        fmt.Println(file.Name(), file.IsDir())
+		if !file.IsDir(){
+           extension:= strings.Split(file.Name(), ".")[1];
+		   if extension == "png" || extension == "jpeg" {
+              picsArr = append(picsArr , root_src + "\\"+file.Name());
+		   } 
+		}
+    }
 
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
-
+	image := canvas.NewImageFromFile(picsArr[0]);
+	tabs := container.NewAppTabs(
+		container.NewTabItem("Tab 1", widget.NewLabel("Hello")),
+		container.NewTabItem("Tab 2", widget.NewLabel("World")),
+	)
+    w.SetContent(container.NewVBox(image, tabs));
 	w.ShowAndRun()
-}
+} 
+
+
+
+	
