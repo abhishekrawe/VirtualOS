5a594ac12b33df75f2b2d7cb013b000fc2856527
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-11-01 16:47:45 +0530

    commit to master

--- MY OS/main.go
+++ MY OS/main.go
@@ -3,10 +3,13 @@ package main
 
 import (
 
-	"fyne.io/fyne"
+	
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/canvas"
 
 )
 
@@ -40,19 +43,35 @@ func main() {
 	})
 
 	btn2 = widget.NewButtonWithIcon("Calculator", theme.ContentAddIcon(), func() {
-		showCalc(myWindow)
+		showCalc()
 	})
 
 	btn3 = widget.NewButtonWithIcon("Gallery App", theme.StorageIcon(), func() {
 		showGalleryApp(myWindow)
 	})
 
-	btn4 = widget.NewButtonWithIcon("Text Editor", theme.HomeIcon(), func() {
-		showTextEditor(myWindow)
+	btn4 = widget.NewButtonWithIcon("Text Editor", theme.DocumentIcon(), func() {
+		showTextEditor()
 	})
 
+	DeskBtn = widget.NewButtonWithIcon("Text Editor", theme.HomeIcon(), func() {
+		showTextEditor(container.NewBorder(panelContent, nil , nil , nil , img))
+	})
+
+	panelContent = container.NewVBox(container.NewGridWithColumns(5, DeskBtn , btn1 , btn2 , btn3 , btn4 ))
 
 
+ myWindow.Resize(fyne.NewSize(1280 , 720))
+ myWindow.CenterOnScreen();
+
+ myWindow.SetContent(
+	 container.NewBorder(panelContent , nil , nil , nil , img ) ,
+ )
+
+
+
+
+   myWindow.ShowAndRun()
 
 
 }
