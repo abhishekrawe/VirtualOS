20e24f0edf2f1020b19f5d2c6f035c7614f85625
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-11-01 15:53:39 +0530

    commit to master

--- MY OS/main.go
+++ MY OS/main.go
@@ -1,4 +1,36 @@
 package main
 
 
+import (
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/v2"
+)
+
+
+var myApp fyne.App = app.New()
+
+
+var myWindow fyne.Window = myApp.NewWindow("My OS")
+
+
+var btn1 fyne.Widget
+
+var btn2 fyne.Widget
+
+var btn3 fyne.Widget
+
+var btn4 fyne.Widget
+
+var img fyne.CanvasObject;
+
+var DeskBtn fyne.Widget
+
+var panelContent *fyne.Container
+
+func main() {
+	myApp.Settings().SetTheme(theme.LightTheme())
+}
+
+
 
