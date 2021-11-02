294ec410865c6b0a0f43b05e3ef669fe9dc0faf7
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-11-02 23:07:26 +0530

    resolved some errror in Gallery Part

--- MY OS/gallery.go
+++ MY OS/gallery.go
@@ -6,33 +6,35 @@ import (
     "log"
 	"strings"
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	//"fyne.io/fyne/v2/widget"
-	//"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
-	//"fyne.io/fyne/v2/theme"
+	
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("Image Viewer")
-	w.Resize(fyne.NewSize(800,630));
-	root_src:="C:\\Users\\HP\\Pictures";
-	files, err := ioutil.ReadDir(root_src);
-	if err != nil {
-        log.Fatal(err)
-    }
+//func main() {
+	// a := app.New()
+	// w := a.NewWindow("Image Viewer")
+	// w.Resize(fyne.NewSize(800,630));
+	// root_src:="C:\\Users\\HP\\Pictures";
+	// files, err := ioutil.ReadDir(root_src);
+	// if err != nil {
+    //     log.Fatal(err)
+    // }
 
-	tabs := container.NewAppTabs(
-		
-	)
+	func showGallery(w fyne.Window){
+		root_src := "gallery";
+		files, err := ioutil.ReadDir(root_src);
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	tabs := container.NewAppTabs()
 
 	for _, file := range files {
         fmt.Println(file.Name(), file.IsDir())
 		if !file.IsDir(){
            extension:= strings.Split(file.Name(), ".")[1];
-		   if extension == "png" || extension == "jpeg" {
+		   if extension == "png" || extension == "jpeg" || extension == "jpeg" {
               
 			  image:=canvas.NewImageFromFile(root_src + "\\"+file.Name());
 			  image.FillMode = canvas.ImageFillOriginal
@@ -48,9 +50,9 @@ func main() {
 	
 		
 	
-	tabs.SetTabLocation(container.TabLocationLeading)
-    w.SetContent(tabs);
-	w.ShowAndRun()
+	tabs.SetTabLocation(container.TabLocationBottom)
+    w.SetContent(container.NewBorder(DeskBtn,nil,nil,nil,tabs),)
+	w.Show()
 } 
 
 
